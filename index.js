d35eec24151278e47669ed7669e3ee7138265606
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-04-26 13:40:01 -0700

    911 data displayed, map not working properly

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
+var request = require('request');
+
 var app = express();
 
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/static'))
+
 app.get('/', function(req, res) {
-	res.send('welcome to seattle 911!');
+	res.render('index');
+});
+
+app.get('/calls', function(req, res) {
+
+	request('https://data.seattle.gov/resource/grwu-wqtk.json?$limit=50&$offset=0', function(err, response, body) {
+		var dataObj = JSON.parse(body);
+		if(!err && response.statusCode === 200) {
+			res.render('calls', {calls: dataObj});
+		} else {
+			res.render('error');
+		}
+	});
 });
 
 var port = 3000;
